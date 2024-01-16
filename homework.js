a643879ca98097397ff44441c3e0054e038c95ed
Alex Bengardt <alexandragorky@yandex.ru>
2024-01-16 13:46:43 +0300

    first commit

---
+++ homework.js
@@ -0,0 +1,13 @@
+let weekdays = {
+	1: "Понедельник",
+	2: "Вторник",
+	3: "Среда",
+	4: "Четверг",
+	5: "Пятница",
+	6: "Суббота",
+	7: "Воскресенье",
+}
+console.log(weekdays);
+console.log(weekdays[4]);
+delete weekdays[5];
+console.log(weekdays);
